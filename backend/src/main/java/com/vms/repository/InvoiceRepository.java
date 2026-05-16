070a6836640eeee993d44db83146eb589cabe7e4
Ektasa <adleyadsion2r@gmail.com>
2026-05-16 22:28:17 +0530

    Vendor

--- backend/src/main/java/com/vms/repository/InvoiceRepository.java
+++ backend/src/main/java/com/vms/repository/InvoiceRepository.java
@@ -1,7 +1,18 @@
+package com.vms.repository;
+
+import com.vms.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
-public interface InvoiceRepository implements JpaRepository<Invoice, Long> {
-    public InvoiceData findByInvoiceNumber(String invoiceNumber);
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+   public Invoice findByInvoiceNumber(String invoiceNumber);
+    public Invoice createInvoice(Invoice invoiceData);
+    public List<Invoice> getAllInvoices();
+     public Invoice getInvoiceById(Long id);
+     public void deleteInvoice(Long id);
     
 }
     
