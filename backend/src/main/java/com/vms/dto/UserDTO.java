beb94aacd70afe0b27f08b54f90b15e649eb7720
Ektasa <adleyadsion2r@gmail.com>
2026-05-18 23:20:14 +0530

    DTO

--- backend/src/main/java/com/vms/dto/UserDTO.java
+++ backend/src/main/java/com/vms/dto/UserDTO.java
@@ -1,21 +1,17 @@
 package com.vms.dto;
 
 import com.vms.entity.User;
-import jdk.jshell.Snippet;
+import lombok.*;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
-
-
-    @lombok.Data
-    @lombok.Builder
-    @lombok.NoArgsConstructor
-    @lombok.AllArgsConstructor
-    public static class userDTO {
-        private Long id;
-        private String email;
-        private String fullName;
-        private User.Role role;
-        private boolean enabled;
-    }
+    private Long id;
+    private String email;
+    private String fullName;
+    private User.Role role;
+    private boolean enabled;
 }
 
