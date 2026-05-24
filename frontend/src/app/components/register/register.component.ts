4fac1ac7c39169b17da189c40186ec534082d186
Ektasa <adleyadsion2r@gmail.com>
2026-05-24 13:06:10 +0530

    Register

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+// @ts-ignore
+import { authService } from '../services/auth.service';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
