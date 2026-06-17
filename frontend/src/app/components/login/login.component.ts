eb14f3111c5096b78262d0fe95767d70e6010d37
Ektasa <adleyadsion2r@gmail.com>
2026-06-17 21:48:57 +0530

    login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { Router } from '@angular/router';
@@ -9,7 +10,7 @@ import {AuthService} from "../../services/auth.service";
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
