320df470bdab9fa4b508e861bf6486875a9de4df
Ektasa <adleyadsion2r@gmail.com>
2026-07-09 21:04:46 +0530

    invoice

--- backend-invoice/src/main/java/com/vms/invoices/config/KafkaConfig.java
+++ backend-invoice/src/main/java/com/vms/invoices/config/KafkaConfig.java
@@ -49,16 +49,14 @@ public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, InvoiceEvent> consumerFactory() {
-        JsonDeserializer<InvoiceEvent> deserializer = new JsonDeserializer<>(InvoiceEvent.class);
-        deserializer.addTrustedPackages("*");
-
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "invoice-service-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        JsonDeserializer<InvoiceEvent> deserializer = new JsonDeserializer<>(InvoiceEvent.class);
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
