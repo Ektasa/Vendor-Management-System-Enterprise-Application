c12787ed77268b041f574d7fdb353809eb574416
Ektasa <adleyadsion2r@gmail.com>
2026-08-09 18:16:47 +0530

    login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -11,7 +11,8 @@ import {AuthService} from "../../services/auth.service";
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   error = '';
